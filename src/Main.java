33be878433c6887fb9429cc393029bafef42933a
VARIANT 7 <Kislenko1950@gmail.com>
2022-12-10 10:29:02 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer();
+        Thread tChatServer = new Thread(chatServer);
+        tChatServer.start();
+
+    }
+}
